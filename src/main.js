b222ab1c2066d488d46ab1e3d5652c50c7011102
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-12 18:51:33 +0200

    installed vue router, imported createRouter and createWebHistory, defined router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
+
+const router = createApp({
+  history: createWebHistory(),
+  routes: [], // we have to tell the router which vue component should be loaded for shich URL
+});
 
 app.mount('#app');
