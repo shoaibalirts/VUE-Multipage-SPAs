4fd647dc2377b84952c35030b20815ce738049b0
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-12 19:20:29 +0200

    added routes to main.js, placed a view-router as a placeholder and verified that url links opened the desired page. Web History is also maintained

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 const app = createApp(App);
 
-const router = createApp({
+const router = createRouter({
   history: createWebHistory(),
-  routes: [], // we have to tell the router which vue component should be loaded for shich URL
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
 });
-
+app.use(router);
 app.mount('#app');
