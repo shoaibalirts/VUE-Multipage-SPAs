01d58516e04d2e63e31740161c94cf10eff97650
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-13 19:14:13 +0200

    add scrollBehavior for good user experience

--- src/main.js
+++ src/main.js
@@ -32,6 +32,12 @@ const router = createRouter({
     { path: '/:notFound(.*)', component: NotFound }, // default (unnamed) router view
   ],
   linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
 });
 app.use(router);
 app.mount('#app');
